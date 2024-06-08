f32664b20ddf9000a4b83463b2645bbdb5594fbd
Anas <anasna6005@gmail.com>
2024-06-08 14:54:36 +0530

    Initial commit

---
+++ server01/index.js
@@ -0,0 +1,39 @@
+const Express = require('express');
+const app = Express();
+
+app.get('/fast-response' , (req,res)=>{
+    const startTime = Date.now();
+    setTimeout(()=>{
+        const endTime = Date.now(); // Record the end time
+        const responseTime = endTime - startTime; // Calculate the response time
+
+        res.json({
+            success:true,
+            message:"Fast response Endpoint",
+            responseTime:`${responseTime} ms`
+        })
+        console.log("Responded in =>" , responseTime)
+    })
+})
+
+app.get('/slow-response' , (req,res)=>{
+    const startTime = Date.now();
+    setTimeout(()=>{
+        const endTime = Date.now(); // Record the end time
+        const responseTime = endTime - startTime; // Calculate the response time
+
+        res.json({
+            success:true,
+            message:"Slow response Endpoint",
+            responseTime:`${responseTime} ms`
+        })
+    },1000)
+})
+
+
+
+app.get('/',(req,res)=>{
+    res.json({message:"This is server 3001"})
+})
+
+app.listen(3001,()=> console.log("Server started at port 3001"))
---
+++ server03/index.js
@@ -0,0 +1,35 @@
+const Express = require('express');
+const app = Express();
+app.get('/fast-response' , (req,res)=>{
+    const startTime = Date.now();
+    setTimeout(()=>{
+        const endTime = Date.now(); // Record the end time
+        const responseTime = endTime - startTime; // Calculate the response time
+
+        res.json({
+            success:true,
+            message:"Fast response Endpoint",
+            responseTime:`${responseTime} ms`
+        })
+    })
+})
+
+app.get('/slow-response' , (req,res)=>{
+    const startTime = Date.now();
+    setTimeout(()=>{
+        const endTime = Date.now(); // Record the end time
+        const responseTime = endTime - startTime; // Calculate the response time
+
+        res.json({
+            success:true,
+            message:"Slow response Endpoint",
+            responseTime:`${responseTime} ms`
+        })
+    },1000)
+})
+
+app.get('/',(req,res)=>{
+    res.json({message:"This is server 3003"})
+})
+
+app.listen(3003,()=> console.log("Server started at port 3003"))
